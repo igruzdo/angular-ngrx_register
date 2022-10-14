863a1ea8fbc6c1eda9033ab0d0ab0b27d6b4d538
Igor Gruzdo <igruzdo@yandex.ru>
2022-10-14 21:30:01 +0300

    added interfacec

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -1,7 +1,8 @@
 import { createAction, props } from "@ngrx/store";
+import { RegisterRequesInterface } from "../../types/registerRequest.interface";
 import { ActionsTypes } from "../actions.types";
 
 export const registerAction = createAction(
         ActionsTypes.REGISTER, 
-        props<{username: string; email: string; password: string}>()
+        props<RegisterRequesInterface>()
     )
---
+++ src/app/auth/types/registerRequest.interface.ts
@@ -0,0 +1,7 @@
+export interface RegisterRequesInterface {
+    user: {
+        email: string;
+        password: string;
+        username: string;
+    }
+}
