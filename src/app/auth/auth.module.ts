1647b08f44ed0c2f0c4f1e5f646295056aaabc21
Igor Gruzdo <igruzdo@yandex.ru>
2022-10-15 16:11:40 +0300

    done 13

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,9 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
 import { RegisterComponent } from "src/app/auth/components/register/register.component";
+import { reducers } from "./store/reducers";
 
 const routes: Routes = [
     {
@@ -17,6 +19,7 @@ const routes: Routes = [
         CommonModule, 
         RouterModule.forChild(routes),
         ReactiveFormsModule,
+        StoreModule.forFeature('auth', reducers)
     ],
     declarations: [RegisterComponent]
 })
